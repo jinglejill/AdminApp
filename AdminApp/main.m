5a70480e981288cca9276b0b83a331850e959b4c
Thidaporn Kijkamjai <jinglejill@me.com>
2018-08-18 07:44:42 +0700

    Initial Commit

---
+++ AdminApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AdminApp
+//
+//  Created by Thidaporn Kijkamjai on 18/8/2561 BE.
+//  Copyright © 2561 Jummum Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
